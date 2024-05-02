e7c740fad17afebbdc82734223a0d1971d679d59
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-02 15:04:55 -0300

    docs: add organizational comments to code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Connection to the database
+
+
+
+//create a connection to the database
+
+
+// config cors
+
+
+// convert data the body a object the js
+
+
+// loading config routes
+
+// put server  listing petitions http
