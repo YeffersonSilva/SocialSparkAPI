6021b11b784b03c673c490fe4a4a0d5ae37e7e45
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-02 15:28:58 -0300

    feat: initialize server and configure middleware - Enable CORS with app.use(cors()) - Parse JSON and URL-encoded data with express middleware

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 //import dependencies
 
 const connection= requiere('./database/connection')
-
+const express = require('express');
+const cors = require('cors');
 
 
 // Connection to the database
 connection();
 
 
-//create a connection to the database
+//create a connection to server
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // config cors
+app.use(cors());
 
-
-// convert data the body a object the js
-
+// convert data the body a object the json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // loading config routes
 
 // put server  listing petitions http
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
