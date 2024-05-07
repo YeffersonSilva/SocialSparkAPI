72bc834a13c9c872dd85d44a867a596f174c4893
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-07 15:23:32 -0300

    feat: create auth middleware for user authentication

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,41 @@
 // imports module
+const jwt = require('jwt-simple');
 
+const moment = require('moment');
 // secret key
+require('dotenv').config();
 
-//function the autehnticate user
+//ket secret
+const SECRET_KEY = process.env.SECRET_KEY_JWT;
+//Middlwares the autehnticate user
+exports.auth = (req, res, next) => {    
 
 
 // check if the token is valid
 
+    if  (!req.headers.authorization) {
+        return res.status(403).send({ status:"eror", message: 'The request does not have the authentication header' });
+    }
+
+    
 // incripted token
 
+    let token = req.headers.authorization.replace(/['"]+/g, '');
+
+    try {
+        var payload = jwt.decode(token, SECRET_KEY);
+        if (payload.exp <= moment().unix()) {
+            return res.status(404).send({ status:"error", message: 'The token has expired' });
+        }
+    }catch (error) {
+        return res.status(404).send({ status:"error", message: 'The token is invalid' });
+    }
+
+
+
 // add data is user request
+
+    req.user = payload;
+    next();
+
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {testUser, register,login} = require('../controllers/user.controllers');
+const { testUser, register, login } = require('../controllers/user.controllers');
+const { auth } = require('../middlewares/auth');
 
 router.get('/user', testUser);
 
 router.post('/register', register);
 
-router.post('/login', login)
+router.post('/login',auth, login)
 
 module.exports = router;
