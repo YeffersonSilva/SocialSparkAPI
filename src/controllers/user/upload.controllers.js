1a4770d23b95f6036c5f673b1ceea428d8e7fffd
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-08 16:57:01 -0300

    feat: configure Multer for file uploads

--- src/controllers/user/upload.controllers.js
+++ src/controllers/user/upload.controllers.js
@@ -2,7 +2,9 @@
 exports.upload = (req, res) => {
     return res.status(200).json({
         message: "Upload image",
-        user: req.user
+        user: req.user,
+        file: req.file,
+        files: req.files
     });
   }
   
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,30 +1,41 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 
-// Import individual controller modules
-//const { testUser } = require('../controllers/user/testUser.controller');
+// Import controllers
 const { register } = require('../controllers/user/register.controllers');
 const { login } = require('../controllers/user/login.controllers');
 const { profile } = require('../controllers/user/profile.controllers');
 const { list } = require('../controllers/user/list.controllers');
 const { update } = require('../controllers/user/update.controllers');
 const { upload } = require('../controllers/user/upload.controllers');
-
 const check = require('../middlewares/auth');
 
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploadPath = path.join(__dirname, '..', 'uploads', 'avatars');
+        if (!fs.existsSync(uploadPath)){
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+        const fileName = "avatar-" + new Date().toISOString().replace(/:/g, '-') + "-" + file.originalname;
+        cb(null, fileName);
+    }
+});
+
+const uploads = multer({ storage: storage });
+
 // User routes
-//router.get('/user', testUser);
-
 router.post('/register', register);
-
 router.post('/login', login);
-
 router.get('/profile/:id', check.auth, profile);
-
 router.get('/list/:page?', list);
-
 router.put('/update', check.auth, update);
-
-router.post('/upload', check.auth, upload);
+router.post('/upload', [check.auth, uploads.single("file0")], upload);
 
 module.exports = router;
