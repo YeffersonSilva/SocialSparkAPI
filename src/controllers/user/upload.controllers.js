ae4e57dce856a24c3681d1130c75d60155f8b79b
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-09 17:27:20 -0300

    feat: add image extension validation in upload feature

--- src/controllers/user/upload.controllers.js
+++ src/controllers/user/upload.controllers.js
@@ -1,5 +1,22 @@
 
 exports.upload = (req, res) => {
+
+    if (!req.file && !req.files) {
+        return res.status(400).json({
+            message: "No files uploaded"
+        });
+    }
+    let images = req.file.originalname;
+
+    const imageSplit = images.split("\.");
+    const extension = imageSplit[imageSplit.length - 1];
+
+    if (extension !== "png" && extension !== "jpg" && extension !== "jpeg") {
+        return res.status(400).json({
+            message: "Invalid file format"
+        });
+    }
+
     return res.status(200).json({
         message: "Upload image",
         user: req.user,
