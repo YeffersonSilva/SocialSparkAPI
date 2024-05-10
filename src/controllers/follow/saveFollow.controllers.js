6d56b49ea1d085b5c1e150c2a9fad533beedfadf
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-10 16:47:53 -0300

    feat: implement functionality to store followed user data

--- src/controllers/follow/saveFollow.controllers.js
+++ src/controllers/follow/saveFollow.controllers.js
@@ -1,14 +1,31 @@
-const Follow = require('../../models/Follow');
-const User = require('../../models/User');
-
+const Follow = require("../../models/Follow");
+const User = require("../../models/User");
 
 exports.saveFollow = (req, res) => {
+  const params = req.body;
+  const indenty = req.user;
 
-    const idUser = req.user.id;
+  let userToFollow = new Follow({
+    user: indenty.id,
+    followed: params.followed,
+});
 
 
+  userToFollow.save((err, followStored) => {
+    if (err)
+      return res
+        .status(500)
+        .send({ message: "Error al guardar el seguimiento" });
+    if (!followStored)
+      return res
+        .status(404)
+        .send({ message: "El seguimiento no se ha guardado" });
 
-
-    res.status(200).send({ message: 'saveFollow controller works' });
-
-}
+    res.status(200).send({
+        message: "saveFollow controller works",
+        indenty: req.user,
+      follow: followStored
+      
+    });
+  });
+};
