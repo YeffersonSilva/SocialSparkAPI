8bda14ab1219a7f947960188fcfe00c90eb69aa8
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-13 14:25:54 -0300

    feat: create route to list user follows

--- src/controllers/follow/followingFollow.controllers.js
+++ src/controllers/follow/followingFollow.controllers.js
@@ -1,6 +1,29 @@
 const Follow = require("../../models/Follow");
 const User = require("../../models/User");
 
+const mongoosePaginate = require("mongoose-pagination");
+
 
 exports.following = (req, res) => {
+    let userId = req.user;
     
+    if (req.params.id) userId = req.params.id;
+
+    let page = 1;
+
+    if (req.params.page) page = req.params.page;
+    
+    const itemsPerPage = 5;
+
+    Follow.find({ user: userId }).exec((err, follows) => {
+
+
+        return res.status(200).send({
+            status: "success",
+            message: "Listado de usuarios que sigo: ",
+            follows
+        });
+        
+    });
+
+}
