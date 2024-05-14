c7dcb01b8aa028b41bb49dd8b6e99487ca5c6482
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-14 14:40:58 -0300

    feat: create Post model for user publications

--- src/models/Publication.js
+++ src/models/Publication.js
@@ -1,2 +1,21 @@
 const {Schema,model} = require('mongoose');
 
+
+
+const PublicationSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    text: {
+        String,
+        require
+    },
+    file: String,
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = model('Publication',PublicationSchema,"publications");
