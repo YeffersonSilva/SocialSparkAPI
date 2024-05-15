e4d46c8c940f74a81cc70a68ff9216edcc944819
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-14 23:39:52 -0300

    feat: add authentication functionality to middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,42 +1,42 @@
-// imports module
 const jwt = require('jwt-simple');
-
 const moment = require('moment');
-// secret key
 require('dotenv').config();
 
-//ket secret
 const SECRET_KEY = process.env.SECRET_KEY_JWT;
-//Middlwares the autehnticate user
-exports.auth = (req, res, next) => {    
 
-
-// check if the token is valid
-
-    if  (!req.headers.authorization) {
-        return res.status(403).send({ status:"eror", message: 'The request does not have the authentication header' });
+exports.auth = (req, res, next) => {
+    // Check if the authorization header exists
+    if (!req.headers.authorization) {
+        return res.status(403).send({
+            status: "error",
+            message: 'The request does not have the authentication header'
+        });
     }
 
-    
-// incripted token
-
+    // Extract and clean the token
     let token = req.headers.authorization.replace(/['"]+/g, '');
 
     try {
+        // Decode the token
         var payload = jwt.decode(token, SECRET_KEY);
+
+        // Check if the token has expired
         if (payload.exp <= moment().unix()) {
-            return res.status(404).send({ status:"error", message: 'The token has expired' });
+            return res.status(401).send({
+                status: "error",
+                message: 'The token has expired'
+            });
         }
+
+        // Attach user data to the request
         req.user = payload;
-    }catch (error) {
-        return res.status(404).send({ status:"error", message: 'The token is invalid' });
+    } catch (error) {
+        return res.status(401).send({
+            status: "error",
+            message: 'The token is invalid'
+        });
     }
 
-
-
-// add data is user request
-
-   
+    // Proceed to the next middleware or route handler
     next();
-
 };
