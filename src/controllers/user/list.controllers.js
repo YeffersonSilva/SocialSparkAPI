4452b8b0168a87754a4ee24ababc9bf2b6742464
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-14 23:55:49 -0300

    refactor: make list.controllers async and enhance documentation

--- src/controllers/user/list.controllers.js
+++ src/controllers/user/list.controllers.js
@@ -1,43 +1,41 @@
 const User = require("../../models/User");
-const followService =require("../../services/followServices")
-
+const followService = require("../../services/followServices");
 const mongosePagination = require("mongoose-pagination");
 
-exports.list = (req, res) => {
-    // Get page
-  
-    const page = req.params.page ? parseInt(req.params.page) : 1;
-  
-    //consult with pagination
-    let itemsPerPage = 5;
-  
-    User.find()
-      .sort("_id")
-      .paginate(page, itemsPerPage, async(err, users, total) => {
-        if (err) {
-          return res.status(500).json({
-            status: "error",
-            message: "Error in the request",
-          });
-        }
-        if (!users) {
-          return res.status(404).json({
-            status: "error",
-            message: "No users to show",
-          });
-        }
-        const followInfo =await followService.followThisUser(req.user.sub, userId);
+exports.list = async (req, res) => {
+  // Get the page number from the request parameters or default to 1
+  const page = parseInt(req.params.page) || 1;
+  const itemsPerPage = 5;
 
-        // Return user data
-        return res.status(200).json({
-          status: "success",
-          users,
-          page,
-          itemsPerPage,
-          total,
-          pages: Math.ceil(total / itemsPerPage),
-          following: followInfo.following,
-          follower: followInfo.follower
-        });
+  try {
+    // Find users with pagination
+    const users = await User.find().sort("_id").paginate(page, itemsPerPage);
+    const total = await User.countDocuments();
+    if (!users.length) {
+      return res.status(404).json({
+        status: "error",
+        message: "No users to show",
       });
-  };
+    }
+
+    // Get information about following and followers
+    const followInfo = await followService.followThisUser(req.user.sub, userId);
+
+    // Return user data with pagination info
+    return res.status(200).json({
+      status: "success",
+      users,
+      page,
+      itemsPerPage,
+      total,
+      pages: Math.ceil(total / itemsPerPage),
+      following: followInfo.following,
+      follower: followInfo.follower,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: "error",
+      message: "Error in the request",
+    });
+  }
+};
