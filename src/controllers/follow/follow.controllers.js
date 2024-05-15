3b9b24d25737e8661cfce98698d97147e9455c98
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-14 23:43:58 -0300

    docs: add comments and documentation to follow and follower management

--- src/controllers/follow/follow.controllers.js
+++ src/controllers/follow/follow.controllers.js
@@ -1,4 +1,42 @@
-const Follow = require('../../models/follow/follow.model');
-const User = require('../../models/user/user.model');
+const Follow = require("../../models/Follow");
+const User = require("../../models/User");
 
+exports.follow = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.id;
 
+  try {
+    // Check if the user to be followed exists
+    const userToFollow = await User.findById(id);
+    if (!userToFollow) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
+    }
+
+    // Check if the follow relationship already exists
+    const existingFollow = await Follow.findOne({ user: userId, followed: id });
+    if (existingFollow) {
+      return res.status(400).json({
+        status: "error",
+        message: "You are already following this user",
+      });
+    }
+
+    // Create a new follow relationship
+    const newFollow = new Follow({ user: userId, followed: id });
+    await newFollow.save();
+
+    return res.status(200).json({
+      status: "success",
+      message: "You are now following this user",
+      follow: newFollow,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Error following the user",
+    });
+  }
+};
